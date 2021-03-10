54314c58e7aa456f24c4c9423742858e80c21a82
dataoutputstream <mb150996@gmail.com>
2021-03-10 21:55:07 +0100

    umi: audio: adjust makefile for new audio configs

--- kona.mk
+++ kona.mk
@@ -46,6 +46,9 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libfmq 
 
+PRODUCT_PACKAGES += \
+    libaudiohal
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_platform_info_intcodec.xml \
