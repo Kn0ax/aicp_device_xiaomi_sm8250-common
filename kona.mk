41001b2a15aca95131c860df5c5f728a7b6e1250
Kn0ax <yasarmalik42@gmail.com>
2021-04-15 10:29:52 +0200

    umi: add prebuilt GcamGo
    source;
    https://www.celsoazevedo.com/files/android/google-camera/f/changelog1529/

--- kona.mk
+++ kona.mk
@@ -55,7 +55,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     Snap \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    GCamGo	
 
 # Configstore
 PRODUCT_PACKAGES += \
